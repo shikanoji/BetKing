e34afb64193143a38423d50fdedb5c1cc9718a73
nguyen.dinh.thach <nguyendinhthach@sun-asterisk.com>
2020-08-10 10:53:53 +0700

    Initial Commit

---
+++ BetKing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BetKing
+//
+//  Created by nguyen.dinh.thach on 8/10/20.
+//  Copyright © 2020 nguyen.dinh.thach. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
